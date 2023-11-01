9ac2d47391fd698a8ac6ea8b6cb55d68d3951461
Julian Cumming <Jcumming5372@conestogac.on.ca>
2023-10-31 20:32:49 -0400

    some minor changes

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,21 +1,30 @@
 package GUI;
+import Abstract_Classes.*;
+import Components.*;
 
 import javax.imageio.metadata.IIOMetadata;
 import javax.swing.*;
 
 public class GUI {
-    JFrame window;
-    ImageIcon logo;
+    private JFrame window;
+    ImageIcon redlight = new ImageIcon(".png");
+    ImageIcon greenlight = new ImageIcon("GreenLight.png");
+    public JLabel StatusLights[] = new JLabel[9];
     public GUI() {
         window = new JFrame();
         window.setTitle("Chernobyl Control Panel");
         window.setSize(1600, 900);
         window.setResizable(false);
         window.setVisible(true);
-        logo = new ImageIcon("NukeIcon.png");
+        ImageIcon logo = new ImageIcon("NukeIcon.png");
         window.setIconImage(logo.getImage());
+        InitStatusIndicators();
     }
-
+    public void InitStatusIndicators() {
+        for (int i = 0; i < 9; i++) {
+            StatusLights[i].setIcon
+        }
+    }
 
 
 
